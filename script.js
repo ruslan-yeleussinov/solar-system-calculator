17de57eabdeaea291ab304632a82a5e1937358be
Ruslan Yeleussinov <ruslan_yeleussinov@mail.ru>
2024-12-10 21:43:51 +0500

    Input diameter of any selected planet or distance of any selected planet from the Sun in any selected measurement unit

--- script.js
+++ script.js
@@ -143,6 +143,8 @@ console.log('Distances array\n', distancesArray);
 
 
 
+
+
 // PLANETS' NAMES AND MEASUREMENT UNITS ARRAYS
 
 const firstArrayOfPlanetsNames = ["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Moon"];
@@ -151,9 +153,11 @@ const unitsArray = ["km", "m", "cm", "mm"];
 
 
 
+
+
 // CALCULATE BUTTONS
 
-// First button calculates new model according to selected planet, its new diameter and selected measurement unit 
+// First button calculates new model according to selected planet, its new diameter in selected measurement unit 
 firstButton.addEventListener("click", () => {
 
   // Every selected planet has its own index that will be used to find the right name and diameter of this planet in the arrays of original diameters
@@ -193,7 +197,7 @@ function calculateNewModelAcordingToNewDiameter(selectedPlanetIndex, newDiameter
   }
   console.log('\nArray of model diameters of the planets in millimeters\n', modelDiametersArray, '\n\n');
 
-  // Calculate new model distances and generate HTML
+  // Calculate new model distances
   for (let i = 0; i < originalDistancesInMillimetersArray.length; i++) {
     const originalDistanceInMillimeters = originalDistancesInMillimetersArray[i];
     const modelDistanceInMillimeters = originalDistanceInMillimeters / scale;
@@ -201,7 +205,7 @@ function calculateNewModelAcordingToNewDiameter(selectedPlanetIndex, newDiameter
   }
   console.log('\nArray of model distances from the Sun in millimeters\n', modelDistancesArray, '\n\n');
 
-  // Calculate new model diameters and generate HTML 
+  // Format new model diameters and generate HTML 
   for (let i = 0; i < modelDiametersArray.length; i++) {
     const modelDiameter = modelDiametersArray[i];
 
@@ -229,7 +233,7 @@ function calculateNewModelAcordingToNewDiameter(selectedPlanetIndex, newDiameter
   }
   console.log('\n');
 
-  // Calculate new model distances and generate HTML
+  // Format new model distances and generate HTML
   for (let i = 0; i < modelDistancesArray.length; i++) {
     const modelDistance = modelDistancesArray[i];
 
@@ -256,5 +260,109 @@ function calculateNewModelAcordingToNewDiameter(selectedPlanetIndex, newDiameter
     }
   }
   console.log('\n');
+}
 
+// Second button new model according to selected planet, its new distance from the Sun in selected measurement unit
+secondButton.addEventListener('click', () => {
+
+  // Every selected planet has its own index that will be used to find the right name and distance of this planet from the Sun in the arrays of original distances
+  const selectedPlanetIndex = document.getElementById("select-distance-planet").value;
+  const selectedPlanet = secondArrayOfPlanetsNames[selectedPlanetIndex];
+  // Get number of new distance
+  const newDistance = Number(document.getElementById("distance-input").value);
+  // Every selected mesurement unit has its own index that will be used to find the right unit and right distances array for further calculations  
+  const selectedUnitIndex = document.getElementById("select-distance-unit").value;
+  const selectedUnit = unitsArray[selectedUnitIndex];
+
+  console.log('\nNEW MODEL\n'); 
+  console.log(`\nSelected planet: ${selectedPlanet}`);
+  console.log(`New distance: ${newDistance} ${selectedUnit}`);
+  
+  calculateNewModelAcordingToNewDistance(selectedPlanetIndex, newDistance, selectedUnitIndex);
+});
+
+function calculateNewModelAcordingToNewDistance(selectedPlanetIndex, newDistance, selectedUnitIndex) {
+  // Create new empty model diameters and distances arrays
+  const modelDiametersArray = [];
+  const modelDistancesArray = [];
+
+  // Calculate new model scale
+  let scale = distancesArray[selectedUnitIndex][selectedPlanetIndex] / newDistance;
+  console.log('Scale: ', scale, '\n');
+
+  // Display this scale on table's header 
+  let modelHeader = document.querySelector(".model-header");
+  modelHeader.innerHTML = "New Model Scale " + "1 : " + scale.toLocaleString();
+
+  // Calculate new model diameters
+  for (let i = 0; i < originalDiametersInMillimetersArray.length; i++) {
+    const originalDiameterInMillimeters = originalDiametersInMillimetersArray[i];
+    const modelDiameterInMillimeters = originalDiameterInMillimeters / scale;
+    modelDiametersArray.push(modelDiameterInMillimeters);
+  }
+  console.log('\nArray of model diameters of the planets in millimeters\n', modelDiametersArray, '\n\n');
+
+  // Calculate new model distances
+  for (let i = 0; i < originalDistancesInMillimetersArray.length; i++) {
+    const originalDistanceInMillimeters = originalDistancesInMillimetersArray[i];
+    const modelDistanceInMillimeters = originalDistanceInMillimeters / scale;
+    modelDistancesArray.push(modelDistanceInMillimeters);
+  }
+  console.log('\nArray of model distances from the Sun in millimeters\n', modelDistancesArray, '\n\n');
+
+  // Format new model diameters and generate HTML 
+  for (let i = 0; i < modelDiametersArray.length; i++) {
+    const modelDiameter = modelDiametersArray[i];
+
+    if (modelDiameter < 10) {
+      console.log(i, 'modelDiameter < 10');
+      document.querySelectorAll(".model-diameter")[i].innerText = modelDiameter.toLocaleString(); 
+      document.querySelectorAll(".unit-diameter")[i].innerText = "mm";
+
+    } else if (modelDiameter >= 10 && modelDiameter < 1000) {
+      console.log(i, 'modelDiameter >= 10 && modelDiameter < 1 000');
+      document.querySelectorAll(".model-diameter")[i].innerText = (modelDiameter / 10).toLocaleString(); 
+      document.querySelectorAll(".unit-diameter")[i].innerText = "cm";
+
+    } else if (modelDiameter >= 1000 && modelDiameter < 1000000) {
+      console.log(i, 'modelDiameter >= 10 && modelDiameter < 1 000 000');
+      document.querySelectorAll(".model-diameter")[i].innerText = (modelDiameter / 1000).toLocaleString(); 
+      document.querySelectorAll(".unit-diameter")[i].innerText = "m";
+
+    } else if (modelDiameter >= 1000000) {
+      console.log(i, 'modelDiameter >= 1 000 000');
+      document.querySelectorAll(".model-diameter")[i].innerText = (modelDiameter / 1000000).toLocaleString(); 
+      document.querySelectorAll(".unit-diameter")[i].innerText = "km";
+
+    }
+  }
+  console.log('\n');
+
+  // Format new model distances and generate HTML
+  for (let i = 0; i < modelDistancesArray.length; i++) {
+    const modelDistance = modelDistancesArray[i];
+
+    if (modelDistance < 10) {
+      console.log(i, 'modelDistance < 10');
+      document.querySelectorAll(".model-distance")[i].innerText = modelDistance.toLocaleString(); 
+      document.querySelectorAll(".unit-distance")[i].innerText = "mm";
+
+    } else if (modelDistance >= 10 && modelDistance < 1000) {
+      console.log(i, 'modelDistance >= 10 && modelDistance < 1 000');
+      document.querySelectorAll(".model-distance")[i].innerText = (modelDistance / 10).toLocaleString(); 
+      document.querySelectorAll(".unit-distance")[i].innerText = "cm";
+
+    } else if (modelDistance >= 1000 && modelDistance < 1000000) {
+      console.log(i, 'modelDistance >= 10 && modelDistance < 1 000 000');
+      document.querySelectorAll(".model-distance")[i].innerText = (modelDistance / 1000).toLocaleString(); 
+      document.querySelectorAll(".unit-distance")[i].innerText = "m";
+
+    } else if (modelDistance >= 1000000) {
+      console.log(i, 'modelDistance >= 1 000 000');
+      document.querySelectorAll(".model-distance")[i].innerText = (modelDistance / 1000000).toLocaleString(); 
+      document.querySelectorAll(".unit-distance")[i].innerText = "km";
+
+    }
+  }
+  console.log('\n');
 }
