bffdc90d1e3fb8a78e1df22d0cd0169a6c9de327
Ruslan Yeleussinov <ruslan_yeleussinov@mail.ru>
2025-01-08 20:33:58 +0500

    Cleaned script.js from unused variables

--- script.js
+++ script.js
@@ -1,79 +1,8 @@
-const firstButton = document.getElementById("calculate-diameter-button");
-const secondButton = document.getElementById("calculate-distance-button");
-
-const originalDiameterInKmSun = Number(document.getElementById("original-diameter-sun").innerText.replace(/ /g, ''));
-const originalDiameterInKmMercury = Number(document.getElementById("original-diameter-mercury").innerText.replace(/ /g, ''));
-const originalDiameterInKmVenus = Number(document.getElementById("original-diameter-venus").innerText.replace(/ /g, ''));
-const originalDiameterInKmEarth = Number(document.getElementById("original-diameter-earth").innerText.replace(/ /g, ''));
-const originalDiameterInKmMars = Number(document.getElementById("original-diameter-mars").innerText.replace(/ /g, ''));
-const originalDiameterInKmJupiter = Number(document.getElementById("original-diameter-jupiter").innerText.replace(/ /g, ''));
-const originalDiameterInKmSaturn = Number(document.getElementById("original-diameter-saturn").innerText.replace(/ /g, ''));
-const originalDiameterInKmUranus = Number(document.getElementById("original-diameter-uranus").innerText.replace(/ /g, ''));
-const originalDiameterInKmNeptune = Number(document.getElementById("original-diameter-neptune").innerText.replace(/ /g, ''));
-const originalDiameterInKmMoon = Number(document.getElementById("original-diameter-moon").innerText.replace(/ /g, ''));
-
-const originalDistanceInKmSun = Number(document.getElementById("original-distance-sun").innerText.replace(/ /g, ''));
-const originalDistanceInKmMercury = Number(document.getElementById("original-distance-mercury").innerText.replace(/ /g, ''));
-const originalDistanceInKmVenus = Number(document.getElementById("original-distance-venus").innerText.replace(/ /g, ''));
-const originalDistanceInKmEarth = Number(document.getElementById("original-distance-earth").innerText.replace(/ /g, ''));
-const originalDistanceInKmMars = Number(document.getElementById("original-distance-mars").innerText.replace(/ /g, ''));
-const originalDistanceInKmJupiter = Number(document.getElementById("original-distance-jupiter").innerText.replace(/ /g, ''));
-const originalDistanceInKmSaturn = Number(document.getElementById("original-distance-saturn").innerText.replace(/ /g, ''));
-const originalDistanceInKmUranus = Number(document.getElementById("original-distance-uranus").innerText.replace(/ /g, ''));
-const originalDistanceInKmNeptune = Number(document.getElementById("original-distance-neptune").innerText.replace(/ /g, ''));
-const originalDistanceInKmMoon = Number(document.getElementById("original-distance-moon").innerText.replace(/ /g, ''));
-
-const modelDiameterSun = document.getElementById("model-diameter-sun").innerText;
-const modelDiameterMercury = document.getElementById("model-diameter-mercury").innerText;
-const modelDiameterVenus = document.getElementById("model-diameter-venus").innerText;
-const modelDiameterEarth = document.getElementById("model-diameter-earth").innerText;
-const modelDiameterMars = document.getElementById("model-diameter-mars").innerText;
-const modelDiameterJupiter = document.getElementById("model-diameter-jupiter").innerText;
-const modelDiameterSaturn = document.getElementById("model-diameter-saturn").innerText;
-const modelDiameterUranus = document.getElementById("model-diameter-uranus").innerText;
-const modelDiameterNeptune = document.getElementById("model-diameter-neptune").innerText;
-const modelDiameterMoon = document.getElementById("model-diameter-moon").innerText;
-
-const unitDiameterSun = document.getElementById("unit-diameter-sun").innerText;
-const unitDiameterMercury = document.getElementById("unit-diameter-mercury").innerText;
-const unitDiameterVenus = document.getElementById("unit-diameter-venus").innerText;
-const unitDiameterEarth = document.getElementById("unit-diameter-earth").innerText;
-const unitDiameterMars = document.getElementById("unit-diameter-mars").innerText;
-const unitDiameterJupiter = document.getElementById("unit-diameter-jupiter").innerText;
-const unitDiameterSaturn = document.getElementById("unit-diameter-saturn").innerText;
-const unitDiameterUranus = document.getElementById("unit-diameter-uranus").innerText;
-const unitDiameterNeptune = document.getElementById("unit-diameter-neptune").innerText;
-const unitDiameterMoon = document.getElementById("unit-diameter-moon").innerText;
-
-const modelDistanceSun = document.getElementById("model-distance-sun").innerText;
-const modelDistanceMercury = document.getElementById("model-distance-mercury").innerText;
-const modelDistanceVenus = document.getElementById("model-distance-venus").innerText;
-const modelDistanceEarth = document.getElementById("model-distance-earth").innerText;
-const modelDistanceMars = document.getElementById("model-distance-mars").innerText;
-const modelDistanceJupiter = document.getElementById("model-distance-jupiter").innerText;
-const modelDistanceSaturn = document.getElementById("model-distance-saturn").innerText;
-const modelDistanceUranus = document.getElementById("model-distance-uranus").innerText;
-const modelDistanceNeptune = document.getElementById("model-distance-neptune").innerText;
-const modelDistanceMoon = document.getElementById("model-distance-moon").innerText;
-
-const unitDistanceSun = document.getElementById("unit-distance-sun").innerText;
-const unitDistanceMercury = document.getElementById("unit-distance-mercury").innerText;
-const unitDistanceVenus = document.getElementById("unit-distance-venus").innerText;
-const unitDistanceEarth = document.getElementById("unit-distance-earth").innerText;
-const unitDistanceMars = document.getElementById("unit-distance-mars").innerText;
-const unitDistanceJupiter = document.getElementById("unit-distance-jupiter").innerText;
-const unitDistanceSaturn = document.getElementById("unit-distance-saturn").innerText;
-const unitDistanceUranus = document.getElementById("unit-distance-uranus").innerText;
-const unitDistanceNeptune = document.getElementById("unit-distance-neptune").innerText;
-const unitDistanceMoon = document.getElementById("unit-distance-moon").innerText;
-
-
-
 // ARRAYS OF ORIGINAL DIAMETERS IN DIFFERENT UNITS
 console.log('ARRAYS OF ORIGINAL DIAMETERS IN DIFFERENT UNITS\n\n');
 
 // create array of original diameters of the planets in kilometers
-const originalDiametersInKilometersArray = [originalDiameterInKmSun, originalDiameterInKmMercury, originalDiameterInKmVenus, originalDiameterInKmEarth, originalDiameterInKmMars, originalDiameterInKmJupiter, originalDiameterInKmSaturn, originalDiameterInKmUranus, originalDiameterInKmNeptune, originalDiameterInKmMoon];
+const originalDiametersInKilometersArray = [1392700, 4878, 12104, 12756, 6779, 139820, 116460, 50724, 49244, 3475];
 console.log('Array of original diameters of the planets in kilometers:\n', originalDiametersInKilometersArray);
 
 // create array of original diameters of the planets in meters
@@ -104,13 +33,11 @@ console.log('Array of original diameters of the planets in millimeters:\n', orig
 diametersArray = [originalDiametersInKilometersArray, originalDiametersInMetersArray, originalDiametersInCentimetersArray, originalDiametersInMillimetersArray];
 console.log('Diameters array\n', diametersArray);
 
-
-
 // ARRAYS OF ORIGINAL DISTANCES IN DIFFERENT UNITS
 console.log('\nARRAYS OF ORIGINAL DISTANCES IN DIFFERENT UNITS\n\n');
 
 // create array of original diameters of the planets in kilometers
-const originalDistancesInKilometersArray = [originalDistanceInKmSun, originalDistanceInKmMercury, originalDistanceInKmVenus, originalDistanceInKmEarth, originalDistanceInKmMars, originalDistanceInKmJupiter, originalDistanceInKmSaturn, originalDistanceInKmUranus, originalDistanceInKmNeptune, originalDistanceInKmMoon];
+const originalDistancesInKilometersArray = [0, 57910000, 108200000, 149600000, 227900000, 778500000, 1429000000, 2875000000, 4497000000, 384403];
 console.log('Array of original distances of the planets in kilometers:\n', originalDistancesInKilometersArray);
 
 // create array of original diameters of the planets in meters
@@ -141,25 +68,17 @@ console.log('Array of original distances of the planets in millimeters:\n', orig
 distancesArray = [originalDistancesInKilometersArray, originalDistancesInMetersArray, originalDistancesInCentimetersArray, originalDistancesInMillimetersArray];
 console.log('Distances array\n', distancesArray);
 
-
-
-
-
 // PLANETS' NAMES AND MEASUREMENT UNITS ARRAYS
 
 const firstArrayOfPlanetsNames = ["Sun", "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Moon"];
 const secondArrayOfPlanetsNames = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune"];
 const unitsArray = ["km", "m", "cm", "mm"];
 
-
-
-
-
 // CALCULATE BUTTONS
 
 // First button calculates new model according to selected planet, its new diameter in selected measurement unit 
+const firstButton = document.getElementById("calculate-diameter-button");
 firstButton.addEventListener("click", () => {
-
   // Every selected planet has its own index that will be used to find the right name and diameter of this planet in the arrays of original diameters
   const selectedPlanetIndex = document.getElementById("select-diameter-planet").value;
   const selectedPlanet = firstArrayOfPlanetsNames[selectedPlanetIndex];
@@ -268,8 +187,8 @@ function calculateNewModelAcordingToNewDiameter(selectedPlanetIndex, newDiameter
 }
 
 // Second button new model according to selected planet, its new distance from the Sun in selected measurement unit
+const secondButton = document.getElementById("calculate-distance-button");
 secondButton.addEventListener('click', () => {
-
   // Every selected planet has its own index that will be used to find the right name and distance of this planet from the Sun in the arrays of original distances
   const selectedPlanetIndex = document.getElementById("select-distance-planet").value;
   const selectedPlanet = secondArrayOfPlanetsNames[selectedPlanetIndex];
